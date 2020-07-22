ba67da5e6d88686bb812e83ce1e7aad07acd1a31
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-07-22 22:46:43 +0300

    edit: sample data
    Added some sample data for further dealing

--- shop.sql
+++ shop.sql
@@ -83,3 +83,117 @@ UPDATE CURRENT_TIMESTAMP
 ALTER TABLE products CHANGE catalog_id catalog_id BIGINT UNSIGNED DEFAULT NULL;
 ALTER TABLE products
 ADD FOREIGN KEY (catalog_id) REFERENCES catalogs (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+/* Adding some sqmple data */
+INSERT INTO `catalogs`
+VALUES (1, 'Процессоры'),
+	(2, 'Материнские платы'),
+	(3, 'Видеокарты'),
+	(4, 'Жесткие диски'),
+	(5, 'Оперативная память');
+INSERT INTO `products`
+VALUES (
+		1,
+		'Intel Core i3-8100',
+		'Процессор для настольных персональных компьютеров, основанных на платформе Intel.',
+		7890.00,
+		1,
+		NULL,
+		NULL
+	),
+	(
+		2,
+		'Intel Core i5-7400',
+		'Процессор для настольных персональных компьютеров, основанных на платформе Intel.',
+		12700.00,
+		1,
+		NULL,
+		NULL
+	),
+	(
+		3,
+		'AMD FX-8320E',
+		'Процессор для настольных персональных компьютеров, основанных на платформе AMD.',
+		4780.00,
+		1,
+		NULL,
+		NULL
+	),
+	(
+		4,
+		'AMD FX-8320',
+		'Процессор для настольных персональных компьютеров, основанных на платформе AMD.',
+		7120.00,
+		1,
+		NULL,
+		NULL
+	),
+	(
+		5,
+		'ASUS ROG MAXIMUS X HERO',
+		'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX',
+		19310.00,
+		2,
+		NULL,
+		NULL
+	),
+	(
+		6,
+		'Gigabyte H310M S2H',
+		'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX',
+		4790.00,
+		2,
+		NULL,
+		NULL
+	),
+	(
+		7,
+		'MSI B250M GAMING PRO',
+		'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX',
+		5060.00,
+		2,
+		NULL,
+		NULL
+	);
+INSERT INTO `users`
+VALUES (
+		1,
+		'Геннадий',
+		'1990-10-05',
+		NULL,
+		NULL
+	),
+	(
+		2,
+		'Наталья',
+		'1984-11-12',
+		NULL,
+		NULL
+	),
+	(
+		3,
+		'Александр',
+		'1985-05-20',
+		NULL,
+		NULL
+	),
+	(
+		4,
+		'Сергей',
+		'1988-02-14',
+		NULL,
+		NULL
+	),
+	(
+		5,
+		'Иван',
+		'1998-01-12',
+		NULL,
+		NULL
+	),
+	(
+		6,
+		'Мария',
+		'1992-08-29',
+		NULL,
+		NULL
+	);
