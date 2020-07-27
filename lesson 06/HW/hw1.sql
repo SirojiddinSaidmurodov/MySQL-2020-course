6ca2087dd6a709ebfd2553aff6f6f3c362710c71
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-07-27 17:23:35 +0300

    add: hw1.sql with transaction example

---
+++ lesson 06/HW/hw1.sql
@@ -0,0 +1,20 @@
+/* В базе данных shop и sample присутствуют одни и те же таблицы
+ учебной базы данных. Переместите запись id = 1 из таблицы shop.users
+ в таблицу sample.users. Используйте транзакции. */
+-- Сначала создадим базу данных sample ...
+DROP DATABASE IF EXISTS sample;
+CREATE DATABASE sample;
+-- и табличку в ней...
+CREATE TABLE sample.users(
+	id SERIAL PRIMARY KEY,
+	name varchar(255) comment 'Имя покупателя',
+	birthday_at DATE comment 'Деь рождения',
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON
+UPDATE CURRENT_TIMESTAMP
+);
+-- Собственно сама транзакция
+START TRANSACTION;
+INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
+COMMIT;
+SELECT * FROM sample.users;
