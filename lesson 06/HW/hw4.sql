e1021f3b57fe255c46aae6c68ea57338119c9f1c
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-07-27 21:26:03 +0300

    add: hw4
    delete all rows except the last 5 added

---
+++ lesson 06/HW/hw4.sql
@@ -0,0 +1,27 @@
+/* Пусть имеется любая таблица с календарным полем created_at.
+ Создайте запрос, который удаляет устаревшие записи из таблицы, 
+ оставляя только 5 самых свежих записей. */
+START TRANSACTION;
+-- готовим таблицу для теста
+CREATE DATABASE IF NOT EXISTS sample;
+DROP TABLE IF EXISTS sample.info;
+CREATE TABLE sample.info(days date);
+use shop;
+SET @i := -1;
+INSERT INTO sample.info
+SELECT *
+FROM (
+        SELECT date('2018-08-01') + INTERVAL @i := @i + 1 DAY AS created_at
+        FROM (
+                SELECT p1.id p1id,
+                    p2.id p2id
+                FROM products p1,
+                    products p2
+            ) AS pr
+        WHERE @i < 30
+    ) s;
+COMMIT;
+-- сам запрос:
+CREATE VIEW lastdata AS SELECT days FROM sample.info ORDER BY days DESC LIMIT 5;
+DELETE FROM sample.info WHERE days NOT IN (SELECT * FROM lastdata);
+SELECT * FROM sample.info i ;
